12214718e6ef37c17358c3c9ea5b255494444a47
Prishelets <a.savoshinskii@gmail.com>
2026-03-08 00:02:09 +0300

    1) добавленн файл .gitattributes для того, чтобы лишнее не утекало в main 2) перестал работать html в test2 3) обновленны картинки 4) попытка разделить js на 2 файла: неудачна

--- TestFiles/test2/functions.js
+++ TestFiles/test2/functions.js
@@ -1,3 +1,15 @@
+const canvas = document.getElementById("canvas");
+canvas.width = 1280;
+canvas.height = 577;
+document.body.appendChild(canvas);
+const ctx = canvas.getContext('2d');
+
+const floor_1 = document.getElementById("floor_1");
+const floor_2 = document.getElementById("floor_2");
+const floor_3 = document.getElementById("floor_3");
+const floor_4 = document.getElementById("floor_4");
+
+
 function line(x1, y1, x2, y2) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -6,31 +18,6 @@ function line(x1, y1, x2, y2) {
 }
 
 
-function forward(distance) {
-    const rad = angle * Math.PI / 180;
-    const newX = x + distance * Math.cos(rad);
-    const newY = y + distance * Math.sin(rad);
-    line(x, y, newX, newY);
-    x = newX;
-    y = newY;
-}
-
-
-function right(deg) {
-    angle = (angle + deg) % 360;
-}
-
-
-async function draw() {
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = 'black';
-    for (let i = 0; i < 500; i++) {
-        forward(i / 5);
-        right(10);
-        await new Promise(resolve => setTimeout(resolve, 10));
-    }
-}
-
 
 function handleClick(buttonElement) {
     const data = buttonElement.value;
@@ -38,5 +25,7 @@ function handleClick(buttonElement) {
     document.getElementById('output').textContent = 'Button value: ' + floor;
 
     if (floor == "1") { ctx.drawImage(floor_1, 0, 0); }
-    else { ctx.drawImage(floor_2, 0, 0); }
+    else if (floor == "2") { ctx.drawImage(floor_2, 0, 0); }
+    else if (floor == "3") { ctx.drawImage(floor_3, 0, 0); }
+    else if (floor == "4") { ctx.drawImage(floor_4, 0, 0); }
 }
--- TestFiles/test2/main.js
+++ TestFiles/test2/main.js
@@ -1,79 +1,47 @@
+import * as functions from './functions.js';
+
+
+
 const canvas = document.getElementById("canvas");
 
 const floor_1 = document.getElementById("floor_1");
 const floor_2 = document.getElementById("floor_2");
 const floor_3 = document.getElementById("floor_3");
+const floor_4 = document.getElementById("floor_4");
 
 const button_1 = document.getElementById("button_1");
 const button_2 = document.getElementById("button_2");
+const button_3 = document.getElementById("button_3");
+const button_4 = document.getElementById("button_4");
 
 
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = 1280;
+canvas.height = 577;
 document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
 button_1.style.position = "absolute";
 button_1.style.top = "100px";
-button_1.style.left = "550px";
+button_1.style.left = "1550px";
 
 button_2.style.position = "absolute";
 button_2.style.top = "150px";
-button_2.style.left = "550px";
+button_2.style.left = "1550px";
+
+button_3.style.position = "absolute";
+button_3.style.top = "200px";
+button_3.style.left = "1550px";
+
+button_4.style.position = "absolute";
+button_4.style.top = "250px";
+button_4.style.left = "1550px";
 
 
 
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-let angle = 0;
-let floor = "1";
+ctx.drawImage(floor_1, 0, 0);
+functions.line(0, 0, 500, 500);
 
-
-function line(x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-}
-
-
-function forward(distance) {
-    const rad = angle * Math.PI / 180;
-    const newX = x + distance * Math.cos(rad);
-    const newY = y + distance * Math.sin(rad);
-    line(x, y, newX, newY);
-    x = newX;
-    y = newY;
-}
-
-
-function right(deg) {
-    angle = (angle + deg) % 360;
-}
-
-
-async function draw() {
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = 'black';
-    for (let i = 0; i < 500; i++) {
-        forward(i/5);
-        right(10);
-        await new Promise(resolve => setTimeout(resolve, 10));
-    }
-}
-
-
-function handleClick(buttonElement) {
-    const data = buttonElement.value;
-    floor = data;
-    document.getElementById('output').textContent = 'Button value: ' + floor;
-
-    if (floor == "1") { ctx.drawImage(floor_1, 0, 0); }
-    else { ctx.drawImage(floor_2, 0, 0); }
-}
-
-
-
-draw();
 button_1.style.visibility = "visible";
 button_2.style.visibility = "visible";
+button_3.style.visibility = "visible";
+button_4.style.visibility = "visible";
