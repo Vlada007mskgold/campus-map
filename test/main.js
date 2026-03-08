cc019cc0c0f7217e5d778f71176ab2dd8dccbd43
Prishelets <a.savoshinskii@gmail.com>
2026-03-08 21:51:35 +0300

    улучшенна читаемость кода

--- test/main.js
+++ test/main.js
@@ -4,31 +4,23 @@ const floor_1 = document.getElementById("floor_1");
 const floor_2 = document.getElementById("floor_2");
 const floor_3 = document.getElementById("floor_3");
 const floor_4 = document.getElementById("floor_4");
+const floors = [floor_1, floor_2, floor_3, floor_4]
 
 const button_1 = document.getElementById("button_1");
 const button_2 = document.getElementById("button_2");
 const button_3 = document.getElementById("button_3");
 const button_4 = document.getElementById("button_4");
-
+const buttons = [button_1, button_2, button_3, button_4];
 
 
 const ctx = canvas.getContext('2d');
 
-button_1.style.position = "absolute";
-button_1.style.top = "100px";
-button_1.style.left = "1550px";
-
-button_2.style.position = "absolute";
-button_2.style.top = "150px";
-button_2.style.left = "1550px";
-
-button_3.style.position = "absolute";
-button_3.style.top = "200px";
-button_3.style.left = "1550px";
-
-button_4.style.position = "absolute";
-button_4.style.top = "250px";
-button_4.style.left = "1550px";
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.position = "absolute";
+    buttons[i].style.top = `${100 + 50 * i}px`;
+    buttons[i].style.left = "1300px";
+    buttons[i].style.visibility = "visible";
+}
 
 
 
@@ -46,16 +38,9 @@ function handleClick(buttonElement) {
     floor = data;
     document.getElementById('output').textContent = 'Button value: ' + floor;
 
-    if (floor == "1") { ctx.drawImage(floor_1, 0, 0); }
-    else if (floor == "2") { ctx.drawImage(floor_2, 0, 0); }
-    else if (floor == "3") { ctx.drawImage(floor_3, 0, 0); }
-    else if (floor == "4") { ctx.drawImage(floor_4, 0, 0); }
+    ctx.drawImage(floors[parseInt(floor) - 1], 0, 0);
 }
 
 
 
 ctx.drawImage(floor_1, 0, 0);
-button_1.style.visibility = "visible";
-button_2.style.visibility = "visible";
-button_3.style.visibility = "visible";
-button_4.style.visibility = "visible";
